af2a771825cc4061df40b942cd9d68160c7db445
dgcezar <diego.cezar13@gmail.com>
2022-09-06 11:15:31 -0300

    primeiro - exercícios 1 e 2 feitos

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,21 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = 32;
+let isMaiorIdade = true;
+let isCursandoOutraFaculdade = false;
 
-if(){
+if (idade >= 18) {
+  console.log("é maior de idade: " + idade);
+} else {
+  console.log("não é maior de idade");
+}
 
-}else{
-	
+if (isMaiorIdade) {
+  console.log("terminou o ensino médio");
+} else {
+  console.log("ainda não terminou o ensino médio");
+}
+
+if (isCursandoOutraFaculdade === false) {
+  console.log("não está cursando faculdade");
+} else {
+  console.log("está cursando faculdade");
 }
